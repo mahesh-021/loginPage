ec6797afcf99b8332b70866de545117949458053
mahesh-021 <maheshravadad03@gmail.com>
2024-10-20 21:50:23 +0530

    commit

---
+++ login/script.js
@@ -0,0 +1,20 @@
+const container = document.querySelector(".container"),
+  pwShowHide = document.querySelectorAll(".showHidePw"),
+  pwFields = document.querySelectorAll(".password"),
+  signUp = document.querySelector(".signup-link"),
+  login = document.querySelector(".login-link");
+
+const inputElement = document.getElementById("myInput");
+const placeholderValue = inputElement.placeholder;
+
+
+// js code to appear signup and login form
+signUp.addEventListener("click", (e) => {
+  e.preventDefault();
+  container.classList.add("active");
+});
+
+login.addEventListener("click", (e) => {
+  e.preventDefault();
+  container.classList.remove("active");
+});
